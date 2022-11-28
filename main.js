e4bce4a7ce17a48ca6983b79acf7e3224da05c14
Joao Garduño <joao.garduno@gmail.com>
2022-11-27 19:07:00 -0600

    Estructura 2: Fusión del Menú en Mobile

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const menuEmail = document.querySelector('.navbar-right');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurger = document.querySelector('.menuImgBurger');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive');
-})
+});
+
+menuBurger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
+});
